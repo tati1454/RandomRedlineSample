5e73858b2d9cd6c96cb2edd6f9eb931828a927ad
tati1454 <45135760+tati1454@users.noreply.github.com>
2021-11-20 11:30:35 -0300

    Update result.cs

--- result.cs
+++ result.cs
@@ -1,7 +1,6 @@
 // Warning: Some assembly references could not be resolved automatically. This might lead to incorrect decompilation of some parts,
 // for ex. property getter/setter access. To get optimal decompilation results, please manually add the missing references to the list of loaded assemblies.
 
-// /home/tati/Documents/MalwareAnalysis/fh/sample2/REDHACK 1.2.36v/array.exe
 // Pruriently, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
 // Global type: <Module>
 // Entry point: Program.Main
